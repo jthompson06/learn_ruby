c955da902a9d272f427e871df80d4e04ec5487b8
Julian <julian.thompson@joinroot.com>
2021-01-08 15:05:08 -0500

    added ex1

---
+++ hello_jtworld/ex1.rb
@@ -0,0 +1,7 @@
+puts "hello world!"
+puts "i am learning code!"
+puts "the dean won't stand for this?"
+puts "i am going rogue"
+puts "I do my own thing"
+puts "I'd much rather you 'not'."
+puts 'I "said" do NOT touch this'
